665905ea4e29a323d8f08771e9a853124b3d3930
yuchong-pan <panyuchong@gmail.com>
2018-05-04 17:59:16 -0700

    Added interpreter of AE

--- ae.rkt
+++ ae.rkt
@@ -23,3 +23,16 @@
                  (parse (third sexp)))]
        [(-) (sub (parse (second sexp))
                  (parse (third sexp)))])]))
+
+;; calc : AE -> number
+;; consumes an AE and computes the corresponding number
+
+(define (calc an-ae)
+  (type-case AE an-ae
+    [num (n) n]
+    [add (l r) (+ (calc l) (calc r))]
+    [sub (l r) (- (calc l) (calc r))]))
+
+(test (calc (parse '3)) 3)
+(test (calc (parse '{+ 3 4})) 7)
+(test (calc (parse '{+ {- 3 4} 7})) 6)
