f9c5431497186d073240ee2b3fc5c39e78cf5285
yuchong-pan <panyuchong@gmail.com>
2018-05-04 18:31:09 -0700

    Added substitution of WAE

--- wae.rkt
+++ wae.rkt
@@ -28,3 +28,33 @@
            (parse named-expr)
            (parse body))]
     [(? symbol? name) (id name)]))
+
+;; subst : WAE symbol WAE -> WAE
+;; substitutes second argument with third argument in first argument
+
+(define (subst expr sub-id val)
+  (type-case WAE expr
+    [num (n) expr]
+    [add (l r) (add (subst l sub-id val)
+                    (subst r sub-id val))]
+    [sub (l r) (sub (subst l sub-id val)
+                    (subst r sub-id val))]
+    [with (bound-id named-expr bound-body)
+          (if (symbol=? bound-id sub-id)
+              expr
+              (with bound-id
+                    named-expr
+                    (subst bound-body sub-id val)))]
+    [id (v) (if (symbol=? v sub-id) val expr)]))
+
+(test (subst (parse '{+ x x}) 'x 5)
+      (add (num 5) (num 5)))
+(test (subst (parse '{+ x {with {x 3} 20}}) 'x 5)
+      (add (num 5)
+           (with 'x (num 3) (num 20))))
+(test (subst (parse '{+ x {with {x 3} x}}) 'x 5)
+      (add (num 5)
+           (with 'x (num 3) (id 'x))))
+(test (subst (parse '{+ x {with {y 3} x}}) 'x 5)
+      (add (num 5)
+           (with 'y (num 3) (num 5))))
