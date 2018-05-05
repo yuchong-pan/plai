b33b9b39ee85c7032b96c68657ed3146c938e0db
yuchong-pan <panyuchong@gmail.com>
2018-05-04 17:47:44 -0700

    Added parse of AE

--- ae.rkt
+++ ae.rkt
@@ -6,3 +6,20 @@
        (rhs AE?)]
   [sub (lhs AE?)
        (rhs AE?)])
+
+;; <AE> ::= <num>
+;;        | {+ <AE> <AE>}
+;;        | {- <AE> <AE>}
+
+;; parse : sexp -> AE
+;; to convert s-expressions into AEs
+
+(define (parse sexp)
+  (cond
+    [(number? sexp) (num sexp)]
+    [(list? sexp)
+     (case (first sexp)
+       [(+) (add (parse (second sexp))
+                 (parse (third sexp)))]
+       [(-) (sub (parse (second sexp))
+                 (parse (third sexp)))])]))
