f604352a96217d3f1f7ba0977f0764cf48ce4695
yuchong-pan <panyuchong@gmail.com>
2018-05-04 18:42:02 -0700

    Added parse of WAE & fixed a bug in substitution

--- wae.rkt
+++ wae.rkt
@@ -41,20 +41,54 @@
                     (subst r sub-id val))]
     [with (bound-id named-expr bound-body)
           (if (symbol=? bound-id sub-id)
-              expr
               (with bound-id
-                    named-expr
+                    (subst named-expr sub-id val)
+                    bound-body)
+              (with bound-id
+                    (subst named-expr sub-id val)
                     (subst bound-body sub-id val)))]
     [id (v) (if (symbol=? v sub-id) val expr)]))
 
-(test (subst (parse '{+ x x}) 'x 5)
+(test (subst (parse '{+ x x}) 'x (num 5))
       (add (num 5) (num 5)))
-(test (subst (parse '{+ x {with {x 3} 20}}) 'x 5)
+(test (subst (parse '{+ x {with {x 3} 20}}) 'x (num 5))
       (add (num 5)
            (with 'x (num 3) (num 20))))
-(test (subst (parse '{+ x {with {x 3} x}}) 'x 5)
+(test (subst (parse '{+ x {with {x 3} x}}) 'x (num 5))
       (add (num 5)
            (with 'x (num 3) (id 'x))))
-(test (subst (parse '{+ x {with {y 3} x}}) 'x 5)
+(test (subst (parse '{+ x {with {y 3} x}}) 'x (num 5))
       (add (num 5)
            (with 'y (num 3) (num 5))))
+(test (subst (parse '{+ x {with {y x} x}}) 'x (num 5))
+      (add (num 5)
+           (with 'y (num 5) (num 5))))
+(test (subst (parse '{+ x {with {x x} x}}) 'x (num 5))
+      (add (num 5)
+           (with 'x (num 5) (id 'x))))
+
+;; calc : WAE -> number
+;; evaluates WAE expression to numbers
+
+(define (calc expr)
+  (type-case WAE expr
+    [num (n) n]
+    [add (l r) (+ (calc l) (calc r))]
+    [sub (l r) (- (calc l) (calc r))]
+    [with (bound-id named-expr bound-body)
+          (calc (subst bound-body
+                       bound-id
+                       (num (calc named-expr))))]
+    [id (v) (error 'calc "free identifier")]))
+
+(test (calc (parse '5)) 5)
+(test (calc (parse '{+ 5 5})) 10)
+(test (calc (parse '{with {x {+ 5 5}} {+ x x}})) 20)
+(test (calc (parse '{with {x 5} {+ x x}})) 10)
+(test (calc (parse '{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}})) 14)
+(test (calc (parse '{with {x 5} {with {y {- x 3}} {+ y y}}})) 4)
+(test (calc (parse '{with {x 5} {+ x {with {x 3} 10}}})) 15)
+(test (calc (parse '{with {x 5} {+ x {with {x 3} x}}})) 8)
+(test (calc (parse '{with {x 5} {+ x {with {y 3} x}}})) 10)
+(test (calc (parse '{with {x 5} {with {y x} y}})) 5)
+(test (calc (parse '{with {x 5} {with {x x} x}})) 5)
