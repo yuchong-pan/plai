8d307057efecb10a00b4be61f69d305975c57c76
yuchong-pan <panyuchong@gmail.com>
2018-05-05 20:46:20 -0700

    Added conditional construct if0 to F1WAE & fixed a bug in subst

--- f1wae.rkt
+++ f1wae.rkt
@@ -5,13 +5,15 @@
 ;;           | {with {<id> <F1WAE>} <F1WAE>}
 ;;           | <id>
 ;;           | {<id> <F1WAE>}
+;;           | {if0 <F1WAE> <F1WAE> <F1WAE>}
 
 (define-type F1WAE
   [num (n number?)]
   [add (lhs F1WAE?) (rhs F1WAE?)]
   [with (name symbol?) (named-expr F1WAE?) (body F1WAE?)]
   [id (name symbol?)]
-  [app (fun-name symbol?) (arg F1WAE?)])
+  [app (fun-name symbol?) (arg F1WAE?)]
+  [if0 (pred F1WAE?) (truth F1WAE?) (falsity F1WAE?)])
 
 (define-type FunDef
   [fundef (fun-name symbol?)
@@ -32,14 +34,18 @@
            (parse body))]
     [(? symbol? name) (id name)]
     [(list (? symbol? fun-name) arg)
-     (app fun-name (parse arg))]))
+     (app fun-name (parse arg))]
+    [(list 'if0 pred truth falsity)
+     (if0 (parse pred)
+          (parse truth)
+          (parse falsity))]))
 
 ;; subst : F1WAE symbol F1WAE -> F1WAE
 ;; substitutes sub-id in expr by val
 
 (define (subst expr sub-id val)
   (type-case F1WAE expr
-    [num (n) n]
+    [num (n) (num n)]
     [add (l r) (add (subst l sub-id val)
                     (subst r sub-id val))]
     [with (bound-id named-expr bound-body)
@@ -51,7 +57,11 @@
     [id (v) (if (symbol=? v sub-id) val (id v))]
     [app (fun-name arg-expr)
          (app fun-name
-              (subst arg-expr sub-id val))]))
+              (subst arg-expr sub-id val))]
+    [if0 (pred truth falsity)
+         (if0 (subst pred sub-id val)
+              (subst truth sub-id val)
+              (subst falsity sub-id val))]))
 
 ;; lookup-fundef : symbol (listof fundef) -> fundef | bool
 ;; returns function of given name in fun-defs, or false if not found
@@ -83,10 +93,22 @@
            (interp (subst (fundef-body the-fun-def)
                           (fundef-arg-name the-fun-def)
                           (num (interp arg-expr fun-defs)))
-                   fun-defs))]))
+                   fun-defs))]
+    [if0 (pred truth falsity)
+         (if (= 0 (interp pred fun-defs))
+             (interp truth fun-defs)
+             (interp falsity fun-defs))]))
 
 (test (interp (parse '{double {double 5}})
               (list (fundef 'double
                             'n
                             (add (id 'n) (id 'n)))))
       20)
+(test (interp (parse '{sum 10})
+              (list (fundef 'sum
+                            'n
+                            (if0 (id 'n)
+                                 (num 0)
+                                 (add (id 'n)
+                                      (app 'sum (add (id 'n) (num -1))))))))
+      55)
