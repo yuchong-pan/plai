2d0b9efdabc68e7a0b56a88c2df6b89af042c085
yuchong-pan <panyuchong@gmail.com>
2018-07-02 15:54:19 -0700

    Added interpreter of RCFAE in Haskell

---
+++ rcfae.hs
@@ -0,0 +1,45 @@
+type Identifier = String
+data Value = NumV Int
+           | FunV (Value -> Value)
+
+type Env = [(Identifier, Value)]
+data FAE = Num Int
+         | Add FAE FAE
+         | Id Identifier
+         | Fun Identifier FAE
+         | App FAE FAE
+         | Rec Identifier FAE FAE
+         | If0 FAE FAE FAE
+
+interp :: FAE -> Env -> Value
+interp (Num n) env = NumV n
+interp (Add lhs rhs) env = addNumbers (interp lhs env) (interp rhs env)
+interp (Id i) env = lookUp i env
+interp (Fun arg body) env =
+    FunV (\ val -> interp body (extend env arg val))
+interp (App funExpr argExpr) env =
+    let FunV funVal = interp funExpr env
+        argVal = interp argExpr env
+    in (funVal argVal)
+interp (Rec name (Fun arg funBody) recBody) env =
+    let extended = extend env
+                          name
+                          (FunV (\ val -> interp funBody (extend extended arg val)))
+    in interp recBody extended
+interp (If0 pred truth falsity) env =
+    let NumV val = interp pred env
+    in
+        if val == 0
+        then interp truth env
+        else interp falsity env
+
+addNumbers :: Value -> Value -> Value
+addNumbers (NumV n1) (NumV n2) = NumV (n1 + n2)
+
+lookUp :: Identifier -> Env -> Value
+lookUp var ((i,v):r)
+    | (var == i) = v
+    | otherwise = lookUp var r
+
+extend :: Env -> Identifier -> Value -> Env
+extend env i v = (i,v):env
